d24cf16e8cc905303cf2810ce11ddc5b0f3fc6da
WooFoo <sujanstha3220@gmail.com>
2023-11-08 20:24:23 +0545

    debounce call back is added

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useDebouncedCallback } from "use-debounce";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
@@ -8,8 +8,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
 	const pathname = usePathname();
 	const { replace } = useRouter();
 
-	function handleSearch(term: string) {
-		console.log(`Searching ....${term}`);
+	const handleSearch = useDebouncedCallback((term) => {
+		console.log(`Searching... ${term}`);
+
 		const params = new URLSearchParams(searchParams);
 		if (term) {
 			params.set("query", term);
@@ -17,8 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 			params.delete("query");
 		}
 		replace(`${pathname}?${params.toString()}`);
-	}
-
+	}, 300);
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
 			<label htmlFor="search" className="sr-only">
