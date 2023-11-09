bc9c07381ce2918886b0624aa07a5805ec25e48c
WooFoo <sujanstha3220@gmail.com>
2023-11-09 21:32:09 +0545

    added login and authentication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,23 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+
+// ...
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData
+) {
+	try {
+		await signIn("credentials", Object.fromEntries(formData));
+	} catch (error) {
+		if ((error as Error).message.includes("CredentialsSignin")) {
+			return "CredentialSignin";
+		}
+		throw error;
+	}
+}
 
 const InvoiceSchema = z.object({
 	id: z.string(),
